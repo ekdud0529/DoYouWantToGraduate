1396f33264eb76e3606703d8a3ab3a7e6bf969bb
dayoung <ekffkddl0529@naver.com>
2020-08-01 21:24:49 +0900

    activity add

--- app/src/main/java/com/test/doyouwanttograduate/MainActivity.kt
+++ app/src/main/java/com/test/doyouwanttograduate/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.test.doyouwanttograduate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Spinner
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val grade = resources.getStringArray(R.array.grade)
+        val semester = resources.getStringArray(R.array.semester)
+
+        val adapter1 = ArrayAdapter<String>(this@MainActivity, android.R.layout.simple_list_item_1, grade)
+        val adapter2 = ArrayAdapter<String>(this@MainActivity, android.R.layout.simple_list_item_1, grade)
+
+        grade_selector.adapter = adapter1
+        semester_selector.adapter = adapter2
+
+        val complete = findViewById<Button>(R.id.complete)
+        complete.setOnClickListener{ view ->
+            val intent = Intent(this@MainActivity, timetable11Activity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/test/doyouwanttograduate/timetable11Activity.kt
@@ -0,0 +1,5 @@
+package com.test.doyouwanttograduate
+
+class timetable11Activity {
+
+}
