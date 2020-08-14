695bc1abba2c6707f843ebb99b4b9288c7074f6c
jeslsy <jeslsy0507@naver.com>
2020-08-15 02:56:09 +0900

    number_activity button event to Main_activity

--- app/src/main/java/com/test/doyouwanttograduate/NumberChoice.kt
+++ app/src/main/java/com/test/doyouwanttograduate/NumberChoice.kt
@@ -1,11 +1,18 @@
 package com.test.doyouwanttograduate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_number_choice.*
 
 class NumberChoice : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_number_choice)
+
+        number_complete.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
