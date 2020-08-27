b5557ae55ae088975165aa24e7723b2a1b14540a
dayoung <ekffkddl0529@naver.com>
2020-08-27 22:41:54 +0900

    add db

--- app/src/main/java/com/test/doyouwanttograduate/numEnt.kt
+++
@@ -1,8 +0,0 @@
-package com.test.doyouwanttograduate
-
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity
-data class numEnt(@PrimaryKey(autoGenerate = true) val id : Int, var num : Int) {
-}
