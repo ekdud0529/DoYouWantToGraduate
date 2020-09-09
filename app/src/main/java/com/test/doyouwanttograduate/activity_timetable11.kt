7a62719e2bcbb7e1038d0e6b92d9c3321fa35098
dayoung <ekffkddl0529@naver.com>
2020-09-09 21:29:19 +0900

    repush

--- app/src/main/java/com/test/doyouwanttograduate/activity_timetable11.kt
+++ app/src/main/java/com/test/doyouwanttograduate/activity_timetable11.kt
@@ -15,6 +15,11 @@ class activity_timetable11 : AppCompatActivity() {
 
         //과목 선택해서 저장한 리스트 정보 받아와 여기 timetable의 리스트뷰에 뿌려야됨 !
 
+        subject_add.setOnClickListener {
+            val intent = Intent(this@activity_timetable11, SubjectListActivity::class.java)
+            startActivity(intent)
+        }
+
       /*
 
 
